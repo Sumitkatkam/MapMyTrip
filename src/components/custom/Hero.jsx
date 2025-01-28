8328c0ce7d44ce40d8f130fce112b128e84275d5
Sumitkatkam <sumitkatkam@gmail.com>
2025-01-28 14:50:07 +0530

    minor changes

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Button } from '../ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Hero = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    console.log("Button clicked! Navigating to /create-trip");
+    navigate('/create-trip');
+  };
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
         <h1 className='font-extrabold text-[48px] text-center mt-10'>
             <span className='text-[#ff3131]'>Discover Your Next Adventure with AI: <br/></span>Personalized Trip Planner ✈</h1>
         <p className='text-xl text-center text-gray-500'>Discover the future of travel planning—personalized, intuitive, and effortless. <br /> Your next adventure is just a click away with our AI-powered guide.</p>
-        <Link to='/create-trip'><Button>Explore Now 🗺️✨</Button></Link>
-        <img src="/LandingPage.png" className='-mt-20'/>
+        <Link to={'/create-trip'}>
+        <Button>Explore Now 🗺️✨</Button>
+        </Link>
+        <img src="/LandingPage.png" className='mt-10'/>
     </div>
   )
 }
